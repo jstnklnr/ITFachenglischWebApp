2b3babec319a9ab0ad79081a4450ad6988653b4c
Eric Naumburger <ericnaumi56@gmail.com>
2021-07-13 11:02:46 +0200

    Added resource to api

--- API/main.py
+++ API/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_restful import Api
 from vocabulary import Vocabulary
+from audio import Audio
+
 from database_interface import Database
 
 app = Flask(__name__)
@@ -8,5 +10,6 @@ api = Api(app)
 
 db = Database("dbstring")
 api.add_resource(Vocabulary, '/Vocabulary')
+api.add_resource(Audio, '/Audio')
 
 app.run('0.0.0.0', port=5000)
