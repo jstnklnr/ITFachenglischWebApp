888fccdc47742da646aea8ce185c1b5481bd96fc
Eric Naumburger <ericnaumi56@gmail.com>
2021-07-13 09:59:02 +0200

    Edited vocabulary

--- API/vocabulary.py
+++ API/vocabulary.py
@@ -2,38 +2,35 @@ from flask_restful import Resource
 from flask import request, jsonify, make_response
 from main import db
 
+import random
+
 class Vocabulary(Resource):
     def get(self):
-        categories = request.headers.get('categories')
+        topic = request.headers.get('topic')
         amount = request.headers.get('amount')
+        lang = request.headers.get('lang')
         
         print(categories)###
         print(amount)###
         
         #header is missing
-        if not categories:
-            return make_response({
-                "Error": "Category is missing."
-                }, 400)
-                
+        if not topic:
+            return {"Error": "Topic is missing."}, 400
         if not amount:
-            return make_response({
-                "Error": "Amount is missing."
-                }, 400)
-                
+            return {"Error": "Amount is missing."}, 400
+        if not lang1:
+            return {"Error": "Fist language is missing."}, 400
+
         #amount not acceptable
         if amount > 100:
-            return make_response({
-                "Error": "Amount is to high."
-                }, 403)
-        
+            return {"Error": "Amount is to high."}, 403
         if amount < 1:
-            return make_response({
-                "Error": "Amount is to low."
-                }, 403)
-                
-        
-                
-        words = db.select("vocabulary", )
+            return {"Error": "Amount is to low."}, 403
+
+        #TODO
+        if topic[0] == "all":
+            words = db.select("vocabulary", ("languages"), where={"languages=": lang})
+        else:
+            words = db.select("vocabulary", )
                                 
         return "hello world"
