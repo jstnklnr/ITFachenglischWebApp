a1425c474bf315337d8e29949d9841931e4a4739
laal3 <79154603+laal3@users.noreply.github.com>
2021-07-14 13:45:17 +0200

    Edited

---
+++ API/phrase.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import reqparse
+from flask_restful import Resource
+
+class Sentence(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument("lang", type = str)
+        self.reqparse.add_argument("trans-lang", type = str)
+        self.reqparse.add_argument("amount", type = int)
+
+    def get(self):
+        args = self.reqparse.parse_args()
+
+        #parameter missing
+        if not args['amount']:
+            return {"Error": "Amount is missing."}, 400
+        if not args['lang']:
+            return {"Error": "Language is missing."}, 400   
+        if not args['trans-lang']:
+            return {"Error": "Translation language is missing."}, 400  
+
+        #amount not acceptable
+        if args['amount'] > 100:
+            return {"Error": "Amount is to high."}, 403
+        if args['amount'] < 1:
+            return {"Error": "Amount is to low."}, 403
+
+        #SQL QUERYS
+        resultList = db.query_dict(f"""
+                                SELECT phrases.phrase,
+                                FROM phrases
+                                JOIN languages ON languages.id = vocabulary.language
+                                WHERE languages.language = ?
+                                ORDER BY random() LIMIT ?
+                                """, tuple(args['lang'] + args['amount']))
+        
+        return resultList, 200
