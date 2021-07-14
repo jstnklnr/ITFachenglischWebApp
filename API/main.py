a79aa236ff15940a5b0cdefb4d369f2db987ad62
laal3 <79154603+laal3@users.noreply.github.com>
2021-07-14 12:11:53 +0200

    Added sentence resource

--- API/main.py
+++ API/main.py
@@ -6,6 +6,7 @@ from flask_restful import Api
 from vocabulary import Vocabulary
 from audio import Audio
 from translation import Translation
+from senctence import Sentence
 
 from database_interface import Database
 
@@ -16,5 +17,6 @@ db = Database(f"{os.path.dirname(__file__)}\\..\\Database\\database.db")
 api.add_resource(Vocabulary, '/Vocabulary')
 api.add_resource(Audio, '/Audio')
 api.add_resource(Translation, '/Translations')
+api.add_resource(Sentence, '/Sentence')
 
 app.run('0.0.0.0', port=5000)
