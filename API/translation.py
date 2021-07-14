d4b3aaf03932839be0cfcbab570a8a2ffece68bb
laal3 <79154603+laal3@users.noreply.github.com>
2021-07-14 12:02:15 +0200

    Code change

--- API/translation.py
+++ API/translation.py
@@ -7,7 +7,6 @@ class Translation(Resource):
         self.reqparse = reqparse.RequestParser
         reqparse.add_argument("word", type = str)
         reqparse.add_argument("languages", type = str)
-        reqparse.add_argument("amount", type = int)
 
     def get(self):
         args = reqparse.parse_args()
@@ -16,14 +15,6 @@ class Translation(Resource):
             return {"Error": "Word is missing."}, 400
         if not args['lang']:
             return {"Error": "Language is missing."}, 400
-        if not args['amount']:
-            return {"Error": "Amount is missing."}, 400
-            
-        #amount not acceptable
-        if args['amount'] > 100:
-            return {"Error": "Amount is to high."}, 403
-        if args['amount'] < 1:
-            return {"Error": "Amount is to low."}, 403
             
         #SQL WHERE Languages
         langs = args['languages'].split(',')
