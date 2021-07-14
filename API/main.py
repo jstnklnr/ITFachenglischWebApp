405bccae1ad6ebf881866efa631011c8af6413f9
laal3 <79154603+laal3@users.noreply.github.com>
2021-07-14 13:46:45 +0200

    Changed sentence resource to phrase

--- API/main.py
+++ API/main.py
@@ -6,7 +6,7 @@ from flask_restful import Api
 from vocabulary import Vocabulary
 from audio import Audio
 from translation import Translation
-from senctence import Sentence
+from phrase import Phrase
 
 from database_interface import Database
 
@@ -17,6 +17,6 @@ db = Database(f"{os.path.dirname(__file__)}\\..\\Database\\database.db")
 api.add_resource(Vocabulary, '/Vocabulary')
 api.add_resource(Audio, '/Audio')
 api.add_resource(Translation, '/Translations')
-api.add_resource(Sentence, '/Sentence')
+api.add_resource(Phrase, '/Phrase')
 
 app.run('0.0.0.0', port=5000)
