104cb2fe06a1d44a14cddd6bd246c0ba47737ddb
Eric Naumburger <ericnaumi56@gmail.com>
2021-07-15 10:04:15 +0200

    Added HTML to website

--- Website/main.py
+++ Website/main.py
@@ -1,12 +1,19 @@
-from flask import Flask, blueprints
+from flask import Flask
+from flask import blueprints
+from flask_session import Session
+
+import cryptography
 
 def create_app():
     app = Flask(__name__)
-
+    app.config["SESSION_PERMANENT"] = False
+    app.config["SESSION_TYPE"] = "filesystem"
+    Session(app)
+    
     from main_routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
 
 app = create_app()
-app.run('0.0.0.0', port=8080)
+app.run(port=8080, ssl_context='adhoc')
--- Website/main_routes.py
+++ Website/main_routes.py
@@ -1,9 +1,42 @@
 from flask import Flask
 from flask import Blueprint
 from flask import render_template
+from flask import session
+from flask import request
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
+    return render_template("home.html")
+
+@main.route('/exercise-selection')
+def exercise_selection():
+    return render_template("exercise_selection.html")
+
+@main.route('/vocabel-test')
+def vocabel_test():
+    session["exercise"] = "vocabulary"
+    return render_template("vocabel_test.html")
+
+@main.route('/audio')
+def audio():
+    session["exercise"] = "audio"
     return "hallo"
+
+@main.route('/phrase')
+def phrase():
+    session["exercise"] = "phrase"
+    return "hallo"
+
+
+
+@main.route('/book-selection')
+def book_selection():
+    return "hallo"
+
+@main.route('/topic-selection')
+def topic_selection():
+    session["exercise"] = request.args.get("exercise")
+    return render_template("topic_selection.html")
+
