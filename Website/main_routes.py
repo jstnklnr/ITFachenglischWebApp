5d384e84b25d588fe62802b2f0ed95d664a13539
Eric Naumburger <ericnaumi56@gmail.com>
2021-07-15 13:01:57 +0200

    Changes

--- Website/main_routes.py
+++ Website/main_routes.py
@@ -5,7 +5,10 @@ from flask import session
 from flask import request
 from flask import redirect
 
+from api_interface import Api
+
 main = Blueprint('main', __name__)
+api = Api("https://localhost:5000")
 
 @main.route('/')
 def index():
@@ -15,7 +18,7 @@ def index():
 def exercise_selection():
     return render_template("exercise_selection.html")
 
-@main.route('/vocabulary') #TODO
+@main.route('/vocabulary')
 def vocabel_test():
     session["exercise"] = "vocabulary"
     return redirect('/book-selection') 
@@ -31,36 +34,33 @@ def phrase():
     return "hallo" 
 
 
-
-
 @main.route('/book-selection') #TODO
 def book_selection():
     if session["exercise"] != "vocabulary":
         return render_template("home.html")
 
-    #sql get book names
+    bookNames = api.getBooks()
         
     return "hallo" #return all books in html
 
-@main.route('/selection') #TODO
+@main.route('/selection')
 def topic_selection():
     if session["exercise"] != "vocabulary":
         return render_template("home.html")
 
     session["book"] = request.args.get("book")
 
-    if "+" in str(session['book']):
-        #api get book topics
-        #num = len of list
-        pass
-    else:
-        #api get book units
-        #num = len of list
-        pass
-
     num = 0
     namelist = []
+    sel = ""
+    if "+" in str(session['book']):
+        namelist = api.getTopics()
+        num = len(topicNames)
+        sel = "Topic"
+    else:
+        namelist = api.getUnits(session["book"])
+        num = len(unitNames)
+        sel = "Unit"
     
-    return render_template("topic_selection.html", num = num, namelist = namelist)
-
+    return render_template("selection.html", num = num, namelist = namelist, sel = sel)
 
