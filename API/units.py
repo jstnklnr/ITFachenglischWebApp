084a1614e6856697efe24dd346eee3dab228edc0
Eric Naumburger <ericnaumi56@gmail.com>
2021-07-15 12:21:20 +0200

    bug fix

--- API/units.py
+++ API/units.py
@@ -10,8 +10,10 @@ class Units(Resource):
         self.reqparser.add_argument("book")
 
     def get(self):
+        args = self.reqparser.parse_args()
+
         if not args["book"]:
             return {"Error": "Book is missing."}, 400
         
         db = static.database
-        return db.query_dict("SELECT units FROM units JOIN books ON book.id = units.book WHERE books.book = ?", args["book"]), 200
+        return db.query_dict("SELECT unit FROM units JOIN books ON books.id = units.book WHERE books.book = ?", [args["book"]]), 200
