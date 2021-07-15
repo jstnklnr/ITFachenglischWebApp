160e4cca1b9817749ec81d56333e0197e10a0fbb
Eric Naumburger <ericnaumi56@gmail.com>
2021-07-15 08:06:57 +0200

    added main routes to main

--- Website/main.py
+++ Website/main.py
@@ -1,6 +1,12 @@
 from flask import Flask, blueprints
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-from main_routes import main as main_blueprint
-app.register_blueprint(main_blueprint)
+    from main_routes import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
+
+app = create_app()
+app.run('0.0.0.0', port=8080)
