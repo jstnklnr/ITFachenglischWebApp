f48fac943f2083be7682fbdb2db07c219c021f2f
Eric Naumburger <ericnaumi56@gmail.com>
2021-07-16 21:35:16 +0200

    Session type change - filesystem to memcache

--- Website/main.py
+++ Website/main.py
@@ -7,7 +7,7 @@ import cryptography
 def create_app():
     app = Flask(__name__)
     app.config["SESSION_PERMANENT"] = False
-    app.config["SESSION_TYPE"] = "filesystem"
+    app.config["SESSION_TYPE"] = "memcached"
     Session(app)
     
     from main_routes import main as main_blueprint
