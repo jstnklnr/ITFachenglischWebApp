f369c79be3cd748f419ba787a521a5c178dfeed0
Eric Naumburger <ericnaumi56@gmail.com>
2021-07-16 21:30:15 +0200

    Frontend changes

--- Website/static/js/readfile.js
+++ Website/static/js/readfile.js
@@ -10,10 +10,10 @@ var font = document.getElementsByClassName("selection_text");
 
 switch (number) {
     case 1:
-        setStyle(square, "17%", "17%");     
+        setStyle(square, "15%", "15%");     
       break;
     case 2:
-        setStyle(square, "5%", "5%");    
+        setStyle(square, "15%", "15%");    
       break;
     case 3:
         setStyle(square, "15%", "15%");     
@@ -22,7 +22,7 @@ switch (number) {
         setStyle(square, "15%", "15%");     
         break;
     case 5:
-        setStyle(square, "10%", "10%");
+        setStyle(square, "13%", "13%");
         break;
     case 6:
         setStyle(square, "10%", "10%"); 
@@ -31,9 +31,26 @@ switch (number) {
         setStyle(square, "10%", "10%");
         break;
     case 8:
-        setStyle(square, "10%", "10%");     
+        setStyle(square, "8%", "8%");     
         break;
-
+    case 9:
+        setStyle(square, "10%", "10%");     
+        break;    
+    case 10:
+        setStyle(square, "10%", "10%"); 
+          break;
+    case 11:
+        setStyle(square, "10%", "10%");
+          break;
+    case 12:
+        setStyle(square, "10%", "10%");     
+          break;
+    case 13:
+        setStyle(square, "10%", "10%");     
+          break;  
+    case 14:
+        setStyle(square, "5%", "5%");     
+          break; 
     default:
       // Anweisungen werden ausgeführt,
       // falls keine der case-Klauseln mit expression übereinstimmt
@@ -59,6 +76,16 @@ switch (number) {
   }
 
 
+
+
+/*
+var div = document.createElement("div");
+div.style.width = "100px";
+div.style.height = "100px";
+div.style.background = "red";
+div.style.color = "white";
+div.innerHTML = "Hello";
+
+document.getElementById("main").appendChild(div);
+*/
   });
-
-
